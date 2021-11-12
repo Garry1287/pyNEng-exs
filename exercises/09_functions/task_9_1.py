563ac187c602676b72c1790ebd15245207e1a387
s007052 <igordzu@yandex.ru>
2021-11-12 14:49:56 +0300

    9_1

--- exercises/09_functions/task_9_1.py
+++ exercises/09_functions/task_9_1.py
@@ -75,3 +75,12 @@ def generate_access_config(intf_vlan_mapping, access_template):
 
     Возвращает список всех портов в режиме access с конфигурацией на основе шаблона
     """
+    access_config = []
+    for intf, vlan in intf_vlan_mapping.items():
+      access_config.append(f'interface {intf}')
+      for command in access_template:
+        if command.endswith('access vlan'):
+           access_config.append(' {} {}'.format(command, vlan))
+        else:
+          access_config.append(' {}'.format(command))
+    return access_config
